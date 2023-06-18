31416ab53ec491eeb7a3ecfbe63e4ca063d7e493
amratef503092 <41952049+amratef503092@users.noreply.github.com>
2023-06-18 09:16:16 +0300

    addd

--- resources/views/hotels/login.blade.php
+++ resources/views/hotels/login.blade.php
@@ -9,11 +9,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://squid-app-im2ht.ondigitalocean.app/'backend/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://squid-app-im2ht.ondigitalocean.app/backend/plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="https://squid-app-im2ht.ondigitalocean.app/'backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="https://squid-app-im2ht.ondigitalocean.app/backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="https://squid-app-im2ht.ondigitalocean.app/'backend/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="https://squid-app-im2ht.ondigitalocean.app/backend/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -78,10 +78,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="https://squid-app-im2ht.ondigitalocean.app/'backend/plugins/jquery/jquery.min.js"></script>
+<script src="https://squid-app-im2ht.ondigitalocean.app/backend/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="https://squid-app-im2ht.ondigitalocean.app/'backend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://squid-app-im2ht.ondigitalocean.app/backend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="https://squid-app-im2ht.ondigitalocean.app/'backend/dist/js/adminlte.min.js"></script>
+<script src="https://squid-app-im2ht.ondigitalocean.app/backend/dist/js/adminlte.min.js"></script>
 </body>
 </html>
